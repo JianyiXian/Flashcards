495d316924e7090d9175944483bd54dafedbea1f
JianyiXian <jenniferxian123@gmail.com>
2023-05-09 15:33:52 -0700

    add previous button and input

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -4,32 +4,15 @@ import FlipCard from './components/FlipCard';
 
 
 const App = () => {
-  const cards = [
-    { id: 0, front: "猫", back: "cat" },
-    { id: 1, front: "狗", back: "dog" },
-    { id: 2, front: "吃", back: "eat" },
-    { id: 3, front: "你好", back: "hello" },
-    { id: 4, front: "再见", back: "bye" },
-    { id: 5, front: "谢谢", back: "Thank you" }
-  ];
-
-  const [currentCardId, setCurrentCardId] = useState(cards[1].id);
-  const handleNextClick = () => {
-    // Generate the nextIndex randomly
-    const nextIndex = Math.floor(Math.random() * cards.length)
-    setCurrentCardId(cards[nextIndex].id)
-  };
-
-  const currentCard = cards.find((card) => card.id === currentCardId)
 
   return (
     <div>
       <img src="https://blog.lingobus.com/wp-content/uploads/2018/12/learn-chinese-in-5-minutes.jpg" alt="" />
       <h1>Guessing Chinese Word</h1>
       <h2>How good of a Chinese learner are you? Test it!!</h2>
-      <h3>Number of cards:{cards.length}</h3>
-      <FlipCard front={currentCard.front} back={currentCard.back} />
-      <button onClick={handleNextClick}>Next</button>
+
+      <FlipCard />
+
     </div>
 
   )
--- flashcards/src/components/FlipCard.jsx
+++ flashcards/src/components/FlipCard.jsx
@@ -2,22 +2,82 @@ import { useState } from 'react'
 import React from "react"
 import './FlipCard.css'
 
-const Flipcard = (props) => {
+const Flipcard = () => {
+
+    const cards = [
+        { id: 0, front: "猫", back: "cat", color: "" },
+        { id: 1, front: "狗", back: "dog", color: "" },
+        { id: 2, front: "吃", back: "eat", color: "" },
+        { id: 3, front: "你好", back: "hello" },
+        { id: 4, front: "再见", back: "bye" },
+        { id: 5, front: "谢谢", back: "Thank you" }
+    ];
+
     const [showBack, setShowBack] = useState(false);
-    const handleClick = () => {
+    const [currentCardId, setCurrentCardId] = useState(cards[0].id);
+    const [currentVal, setCurrentVal] = useState('')
+
+    const handleCardClick = () => {
         setShowBack(!showBack)
     }
 
+    const handleNextClick = () => {
+        setShowBack(false)
+        const nextIndex = currentCardId + 1
+        setCurrentCardId(cards[nextIndex].id)
+
+    };
+
+    const handlePreviousClick = () => {
+        setShowBack(false)
+        const prevIndex = currentCardId - 1
+        setCurrentCardId(cards[prevIndex].id)
+    };
+
+    const handleRamdomClick = () => {
+        setShowBack(false)
+        // Generate the nextIndex randomly
+        const randomIndex = Math.floor(Math.random() * cards.length)
+        setCurrentCardId(cards[randomIndex].id)
+
+    };
+
+    const checkAnswer = () => {
+        if (currentVal.toLowerCase() == currentCard.back) {
+            alert("Correct")
+        }
+        else {
+            alert("Wrong")
+        }
+    };
+
+    const handleChange = (e) => {
+        setCurrentVal(e.target.value)
+    };
+
+    const currentCard = cards.find((card) => card.id === currentCardId)
+
     return (
-        <div className='card-grid' onClick={handleClick}>
-            <div className={`card ${showBack ? "flip" : ""}`}>
-                <div className='front'>
-                    <h1>{props.front}</h1>
-                </div>
-                <div className='back'>
-                    <h1>{props.back}</h1>
+        <div>
+
+            <div className='card-grid' onClick={handleCardClick}>
+                <div className={`card ${showBack ? "flip" : ""}`}>
+                    <div className='front'>
+                        <h1>{currentCard.front}</h1>
+                    </div>
+                    <div className='back'>
+                        <h1>{currentCard.back}</h1>
+                    </div>
                 </div>
             </div>
+            <div>
+                <input type="text" value={currentVal} placeholder='Guess the Chinese meaning...' onChange={handleChange} className='textbox' />
+                <button onClick={checkAnswer}>Check answer</button>
+            </div>
+            <h3>{currentCardId + 1}/{cards.length}</h3>
+            <button onClick={handlePreviousClick} disabled={currentCardId - 1 < 0 ? true : ""}>Previous</button>
+            <button onClick={handleNextClick} disabled={currentCardId + 1 === cards.length ? true : ""}>Next</button>
+            <button onClick={handleRamdomClick}>Shuffle</button>
         </div>
     )
 }
